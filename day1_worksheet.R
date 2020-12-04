29fb9051b4dca755357909682578d49476cf0212
helixjason <jasonw@helixdc.com>
2020-12-03 20:44:05 -0700

    Initial Commit

---
+++ day1_worksheet.R
@@ -0,0 +1,32 @@
+library(tidyverse)
+
+data <- read.csv(file = "../adventofcode/day1_data-single-c.csv",
+                 header = FALSE)
+
+
+test <- matrix(data, ncol = 200, nrow = 200)
+
+test <- matrix(data)
+
+test2 <- t(data)
+
+sort <- data %>%
+    arrange(V1) %>%
+    mutate(
+        V2 = 2020 - V1,
+        V3 = match(V1, V2),
+        
+    )
+
+sort$V4 <- c(sort$V1[-1],NA)
+
+sort <- sort %>%
+    mutate(
+        V5 = 2020-V1-V4
+    )
+
+p1 <- sort[-c(146:200)]
+
+ sort2 <-  head(sort, 73)   
+
+  
